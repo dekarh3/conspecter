d204e9edb0bba80d7e08a2407b6c93cfea703b70
dekarh <dekarh3@gmail.com>
2023-02-22 22:58:04 +0400

    v 0.1.1 от 22.02.2023:   в классе MyTreeView     - добавил функции has_child, delete_node, child_list     - переимеовал node_ids в uid2id     - поправил тип nodes   в классе MyGrid:     - добавил блок редактирования дерева тэгов   расширил дерево в тестовой бд

--- main.py
+++ main.py
@@ -39,74 +39,152 @@ class MyTreeView(TreeView):
         #with open(os.path.join(self.main_dir_path, 'mytetra.xml'), 'r') as read_file:
         #    main_file = read_file.read()        
         #self.main_tree = xmltodict.parse(main_file)
-        self.node_ids = {}
-        self.nodes = []
+        self.uid2id = {}
+        self.nodes = {}
         self.populate_tree_view(None, 0)#, self.main_tree['root']['content'])
 
-    def populate_tree_view(self, parent, node):
+    def populate_tree_view(self, parent, node_id):
         if parent is None:
             tree_node = self.add_node(MyTreeViewLabel(text='', is_open=True, external_id=0))
-            self.node_ids[tree_node.uid] = 0
-            self.nodes.append(tree_node)
+            self.uid2id[tree_node.uid] = 0
+            self.nodes[0] = tree_node
         else:
-            cur.execute('SELECT id, name FROM tags WHERE id = ?;', (node,))
+            cur.execute('SELECT id, name FROM tags WHERE id = ?;', (node_id,))
             request = cur.fetchone()
             tree_node = self.add_node(MyTreeViewLabel(text=request[1], is_open=True,
                                                            external_id=request[0]), parent)
-            self.node_ids[tree_node.uid] = request[0]
-            self.nodes.append(tree_node)
-        cur.execute('SELECT id, name FROM tags WHERE parent = ?;', (node,))
+            self.uid2id[tree_node.uid] = request[0]
+            self.nodes[request[0]] = tree_node
+        cur.execute('SELECT id, name FROM tags WHERE parent = ?;', (node_id,))
         request = cur.fetchall()
         for child_node in request:
             self.populate_tree_view(tree_node, child_node[0])
-
     def depopulate_tree_view(self):
         for node in self.nodes:
-            self.remove_node(node)
-        self.nodes = []
+            self.remove_node(self.nodes[node])
+        self.nodes = {}
         self.node_ids = {}
-
     def reload_tree(self, main_dir_path):
         #with open(os.path.join(main_dir_path, 'mytetra.xml'), 'r') as read_file:
         #    main_file = read_file.read()
         #self.main_tree = xmltodict.parse(main_file)
         self.depopulate_tree_view()
         self.populate_tree_view(None) #, self.main_tree['root']['content'])
-
+    def has_child(self, node_id):
+        cur.execute('SELECT id, name FROM tags WHERE parent = ?;', (node_id,))
+        request = cur.fetchall()
+        return len(request) > 0
+    def delete_node(self, node_id):
+        cur.execute('DELETE FROM tags WHERE id = ?;', (node_id,))
+        conn.commit()
+        self.reload_tree()
+    def child_list(self, node_id):
+        node_ids = []
+        for node in self.iterate_all_nodes(self.nodes[node_id]):
+            node_ids.append(self.uid2id[node.uid])
+        return node_ids
 
 class MyBoxLayout(BoxLayout):
     background_color = ColorProperty() # The ListProperty will also work.
 
 
 class LoadDialog(FloatLayout):
-    load_mytetra = ObjectProperty(None)
+    loaddb = ObjectProperty(None)
     cancel = ObjectProperty(None)
 
 
 class MyGrid(Widget):
     tag = ObjectProperty(None)
     mention = ObjectProperty(None)
-    def dismiss_popup(self):
-        self._popup.dismiss()
+    def __init__(self, **kwargs):
+        super(MyGrid, self).__init__(**kwargs)
+        self.tvedit_regim = 'Добавление'
+        self.tvedit_current_id = 0
+        self.tvedit_captured_id = -1
+    def btn_tvedit_change_click(self):
+        '''Переключение режимов редактирования дерева'''
+        if self.tvedit_regim == 'Добавление':
+            self.tvedit_regim = 'Редактирование'
+            self.ids.btn_tvedit_change.text = self.tvedit_regim
+            self.ids.btn_tvedit_minus.disabled = True
+            self.ids.btn_tvedit_minus.text = ''
+            self.ids.btn_tvedit_plus.disabled = True
+            self.ids.btn_tvedit_plus.text = 'ok'
+            self.ids.tvedit_text.text = self.tag.nodes[self.tvedit_current_id].text
+            self.ids.tvedit_text.readonly = False
+        elif self.tvedit_regim == 'Редактирование':
+            self.tvedit_regim = 'Удаление'
+            self.ids.btn_tvedit_change.text = self.tvedit_regim
+            self.ids.btn_tvedit_minus.disabled = True
+            self.ids.btn_tvedit_minus.text = ''
+            self.ids.btn_tvedit_plus.disabled = self.tag.has_child(self.tvedit_current_id)
+            self.ids.btn_tvedit_plus.text = '-'
+            self.ids.tvedit_text.text = self.tag.nodes[self.tvedit_current_id].text
+            self.ids.tvedit_text.readonly = True
+        elif self.tvedit_regim == 'Удаление':
+            self.tvedit_regim = 'Перенос'
+            self.ids.btn_tvedit_change.text = self.tvedit_regim
+            self.ids.btn_tvedit_minus.disabled = False
+            self.ids.btn_tvedit_minus.text = 'ok'
+            self.ids.btn_tvedit_plus.disabled = True
+            self.ids.btn_tvedit_plus.text = '>'
+            self.ids.tvedit_text.text = ''
+            self.ids.tvedit_text.readonly = True
+        else:
+            self.tvedit_regim = 'Добавление'
+            self.ids.btn_tvedit_change.text = self.tvedit_regim
+            self.ids.btn_tvedit_minus.disabled = True
+            self.ids.btn_tvedit_minus.text = ''
+            self.ids.btn_tvedit_plus.disabled = True
+            self.ids.btn_tvedit_plus.text = '+'
+            self.ids.tvedit_text.text = ''
+            self.ids.tvedit_text.readonly = False
+
+    def btn_tvedit_minus_click(self):
+        if self.tvedit_regim == 'Перенос':
+            if self.tvedit_captured_id < 0:
+                self.tvedit_captured_id = self.tvedit_current_id
+                self.ids.tvedit_text.text = self.tag.nodes[self.tvedit_current_id].text
+                self.ids.btn_tvedit_minus.text = '<'
+            else:
+                self.ids.tvedit_text.text = ''
+                self.ids.btn_tvedit_plus.disabled = True
+                self.ids.btn_tvedit_minus.text = 'ok'
+                self.ids.btn_tvedit_minus.disabled = False
+    def btn_tvedit_plus_click(self):
+        pass
     def tv_touch(self, value):
-        print(self.tag.node_ids[value])
-    def spinner_clicked(self, value):
+        self.tvedit_current_id = self.tag.uid2id[value]
+        if self.tvedit_regim == 'Редактирование' or self.tvedit_regim == 'Удаление':
+            self.ids.tvedit_text.text = self.tag.nodes[self.tvedit_current_id].text
+        elif self.tvedit_regim == 'Перенос':
+            if self.tvedit_current_id != self.tvedit_captured_id and self.tvedit_captured_id > -1 \
+                    and self.tvedit_captured_id in self.tag.child_list(self.tvedit_current_id):
+                self.ids.btn_tvedit_plus.disabled = False
+            else:
+                self.ids.btn_tvedit_plus.disabled = True
+        if self.tvedit_regim == 'Удаление':
+            self.ids.btn_tvedit_plus.disabled = self.tag.has_child(self.tvedit_current_id)
+    def spn_lecture_click(self, value):
         self.ids.file_id_time.text = value
         self.ids.transcript_text.text = f'You Selected: {value}'
-    def button_clicked(self, value):
+    def btn_conspect_click(self, value):
         self.ids.file_id_time.text = value
         self.ids.transcript_text.text = f'You Selected: {value}'
-    def show_load(self):
-        content = LoadDialog(load_mytetra=self.load_mytetra, cancel=self.dismiss_popup)
+    def cancel_dialog(self):
+        self._popup.dismiss()
+    def show_loaddb_dialog(self):
+        content = LoadDialog(loaddb=self.loaddb, cancel=self.cancel_dialog)
         self._popup = Popup(title="Выбрать mytetra.xml", content=content,
                             size_hint=(0.9, 0.9))
         self._popup.open()
-    def load_mytetra(self, path, filename):
+    def loaddb(self, path, filename):
+        ''' Актуализировать импорт из db'''
         if os.path.basename(os.path.join(path, filename[0])) == 'mytetra.xml':
             main_dir_path = os.path.dirname(os.path.join(path, filename[0]))
-            self.tag.reload_tree(main_dir_path)
+            #self.tag.reload_tree(main_dir_path)
             q=0
-        self.dismiss_popup()
+        self.cancel_dialog()
 
 
 class TrainerApp(App): # <- Main Class
@@ -191,9 +269,11 @@ if __name__ == "__main__":
         cur.executemany('INSERT INTO tags VALUES(?, ?, ?, ?);', [(1, 0, 'юриспруденция', 'q1q1'),
                                                                  (2, 0, 'программирование', 'q1q1'),
                                                                  (3, 1, 'вексельное право', 'q1q1'),
-                                                                 (4, 2, 'ELMA365','q1q1')])
+                                                                 (4, 2, 'ELMA365','q1q1'),
+                                                                 (5, 3, 'выгодоприобретатель','q1q1'),
+                                                                 (6, 5, 'с оборотом','q1q1')])
         conn.commit()
-        conn.close()
+        #conn.close()
     else:
         if os.path.exists(os.path.join(APP_PATH, 'main.db')):
             conn = sqlite3.connect(os.path.join(APP_PATH, 'main.db'))
